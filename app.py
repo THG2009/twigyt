a97b1c5b2752e9a63d83ed60d14cec1fed254ac0
BuildTools <unconfigured@null.spigotmc.org>
2024-05-19 21:27:48 -0500

    init

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+def ferblatin(word: str):
+    first_char = word[0]
+    word = word[1:]
+    final = f'{word}{first_char}erb'
+    return final
+
+
+def translate(word: str):
+    words = word.split()
+
+    modified_words = [ferblatin(word) for word in words]
+
+    modified_sentence = ' '.join(modified_words)
+    return modified_sentence
+
+
 @app.route('/')
-def home_redirect():
-    return redirect(url_for('home'))
-
-
-@app.route('/home')
-def home():
+def index():
     return render_template('home.html')
 
 
-@app.route('/projects')
-def projects():
-    return render_template('projects/projects.html')
-
-
-@app.route('/projects/alpha-apis')
-def alpha_apis():
-    return render_template('projects/alpha-apis.html')
+@app.route('/translated', methods=['POST'])
+def translated():
+    translated_word = request.form['input']
+    return render_template('translated.html', word=translate(translated_word))
 
 
 if __name__ == '__main__':
