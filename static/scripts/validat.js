6036a7464aba0df657afcaa7a42ebfdcc58ea274
BuildTools <unconfigured.spigotmc.org>
2024-05-20 16:05:05 -0500

    Uplouded for remote editing

---
+++ static/scripts/validat.js
@@ -0,0 +1,8 @@
+const textbox = document.getElementById("inputbox")
+const button = document.getElementById("SUBMIT")
+
+button.addEventListener("click", function () {
+   const text = textbox.innerHTML()
+
+   text.
+})
